d7caeb3c73911bcd163f19ad4a446ae350b884d1
Yasel Rubio Ribeaux <Yasel.RubioRibeaux@web.de>
2024-11-30 09:13:22 +0100

    Add files via upload

---
+++ Colecciones_1.py
@@ -0,0 +1,10 @@
+#Ejercicio 1 - Eliminar los elemenos repetidos de una lista
+
+lista = [1,2,3,"Alejandro",2,2,1,"Alejandro",3]
+
+
+lista = list(set(lista))
+
+print(f"Esta es la lista {lista}")
+
+ 
